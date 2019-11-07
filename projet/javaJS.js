c5a0af08f055cb511200aea7244e55f315175e08
johnnyvaca <Johnny.VACA-JARAMILLO@cpnv.ch>
2019-11-07 15:11:32 +0100

    wip ussers

--- projet/javaJS.js
+++ projet/javaJS.js
@@ -28,23 +28,56 @@ function  url() {
 return extraction;
 }
 
-
-i = 0;
+	
 	function enregistrer() {
 
-        var parent = document.getElementById('lVoyageurs');
-        var lignes = document.getElementById('lVoyageurs').rows;
-        var Ldonne = document.getElementById('lVoyageurs').rows[0].cells.length;
+		longeurTr = document.getElementById('lVoyageurs').rows[0].cells.length;
+		newtr = document.createElement('tr')
 
-        console.log(rows);
-        for (i = 0; i < Ldonne; i++)
-        {
-            var Ldonnee = document.getElementById('lVoyageurs').rows[i].cells.length;
-            var input = document.createElement('input');
-            input.setAttribute("placeholder", "nom");
-            parent.appendChild(input);
-        }
+			for(i = 0; i < longeurTr; i++)
+			{
+				newId = document.createElement('td');
+				newInput = document.createElement('input');
+				newInput.type = "text";
+				newId.appendChild(newInput);
+				newtr.appendChild(newId);
 
+			}
+		lVoyageurs.appendChild(newtr)
+	
+
+/*
+		var rows = document.getElementById('lVoyageurs').rows;
+		
+		for (var i = 0; i < 1; i++) { // rows.length
+			var row = rows[i];
+			for (var j = 0; j < 1 ; j++) { //row.cells.length
+				var input = document.createElement('input');
+				input.type = "text";
+				row.cells[i].appendChild(input); //
+			};
+		}; 
+
+
+	*/	
+		
+
+
+		/*
+				var parent = document.getElementById('lVoyageurs');
+				var lignes = document.getElementById('lVoyageurs').rows;
+				var Ldonne = document.getElementById('lVoyageurs').rows[0].cells.length;
+				var fils = document.getElementById('lignes');
+				//var Ldonnee = document.getElementById('lVoyageurs').rows[i].cells.length;
+				console.log(rows);
+				for (i = 0; i < Ldonne; i++)
+				{
+					var input = document.createElement('input');
+					input.setAttribute("placeholder", "nom "+i);
+					fils.appendChild(input);
+				}
+				fils.appendChild(input);
+		*/
 }
 
 
