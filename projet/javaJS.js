f7c4dae876298836f5b5d71218826a532c7bca40
johnnyvaca <Johnny.VACA-JARAMILLO@cpnv.ch>
2019-11-08 16:57:42 +0100

    delete wip

--- projet/javaJS.js
+++ projet/javaJS.js
@@ -39,27 +39,76 @@ function etatDelete() {
     deleteP.disabled = true;
     addP.disabled = false;
     val = ajouter();
-    // line = lVoyageurs.rows[0].cells[5].children;
-   // alert(line);
-    line2 = lVoyageurs.children[0].children[0].children[0].innerText;
-  //  donnees = line.children;
-    console.log("line : " + val.rows);
-    console.log("");
-    for (i = 0; i < 4; i++) {
-/*
-        lVoyageurs.children[i].children[5].children[i].style.visibility = "visible";
-        lVoyageurs.children[i].children[5].children[i].value = "hello";
-        lVoyageurs.children[i].children[5].children[i].innerHTML = "hello";
-        lVoyageurs.children[i].children[5].children[i].innertext = "hello";
-*/
-        console.log("longeur : "+ line2 +"\n");
+
+    longeurTd = document.getElementById('lVoyageurs').rows[0].cells.length;
+    oTR = document.getElementById('lVoyageurs').rows;
+    longeurTr = document.getElementById('lVoyageurs').rows.length;
+    dernierTr = longeurTr - 1;
+    dernierTd = longeurTd;
+    selectionDelete = document.getElementById('lVoyageurs').rows[dernierTr].cells[5];
 
 
 
+    for (i = 0; i < longeurTd + 1; i++) {
+    }
 
+  //  if (longeurTr !== 0) {
+        //document.getElementById('lVoyageurs')
+    //    lVoyageurs.rows[i].cells[5].children[i].style.visibility = "visible";
+     //   selectionDelete.style.visibility = "visible";
+   // }
+   console.log("longeur : "+ longeurTr);
+
+    for(i=1;i<longeurTr+1;i++){
+        
+        oTD = oTR[i].getElementsByTagName('td');
+        oTD[5].style.visibility = "visible";
+        oBTN = oTD[5].getElementsByTagName('button');
+        oBTN[0].innerText = "-";
+        oBTN[0].value = "-";
+        oBTN[0].addEventListener("click",killLine);
+      
 
     }
 
+
+
+/*        if (i === 3) {
+            newtd = document.createElement('td');
+            newSelect = document.createElement('select');
+            newOption = document.createElement('option');
+            newOption.value = "hello"
+            newOption.innerText = "Type pièce d'identité"
+            newSelect.appendChild(newOption)
+            newtd.appendChild(newSelect)
+            newtr.appendChild(newtd);
+        } else if (i < longeurTd) {
+            newtd = document.createElement('td');
+            newInput = document.createElement('input');
+            newInput.type = "text";
+            newtd.appendChild(newInput);
+
+            console.log("input\n")
+
+            newtr.appendChild(newtd);
+        }
+
+
+        if (i === longeurTd) {
+            newtd = document.createElement('td');
+            newBouton = document.createElement('button');
+            newBouton.value = "+"
+            newBouton.innerText = "+"
+            newBouton.addEventListener("click", ajouter)
+      //      console.log("bouton\n")
+            newtd.appendChild(newBouton);
+
+
+        }
+*/
+
+
+   
 
 
 }
@@ -69,9 +118,17 @@ function etatDelete() {
 
 function killLine(event) // Le paramètre event représente l'événement qui a causé l'appel à la fonction
 {
+    longeurTd = document.getElementById('lVoyageurs').rows[0].cells.length;
+
+    longeurTr = document.getElementById('lVoyageurs').rows.length;
+    dernierTr = longeurTr - 1;
+    dernierTd = longeurTd;
+    selectionDelete = document.getElementById('lVoyageurs').rows[dernierTr].cells[dernierTd];
+    
     cmdMoin = event.target // La cible ('target') de l'événement est le bouton (un élément du DOM)
-    td = cmdMoin.parentNode // td est la cellule dans laquelle le bouton se trouve
-    tr = td.parentNode // tr est la ligne ('row') dans laquelle la cellule se trouve
+    btn = cmdMoin.parentNode // td est la cellule dans laquelle le bouton se trouve
+    td = btn.parentNode // tr est la ligne ('row') dans laquelle la cellule se trouve
+    tr = td.parentNode
     tbody = tr.parentNode // table est la table dans laquelle la ligne se trouve
     tbody.removeChild(tr) // On enlève la ligne de la table
 }
